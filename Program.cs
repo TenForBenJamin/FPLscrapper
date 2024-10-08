ec01189782c0b755cfd11267b36e1639ef36061c
TenForBenJamin <73674410+TenForBenJamin@users.noreply.github.com>
2024-10-08 23:56:35 +0200

    exception handling

--- Program.cs
+++ Program.cs
@@ -4,29 +4,74 @@ using System;
 using System.IO;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 using System.Text.Json;
+using System.Diagnostics.Metrics;
 
 public class FolderManager
 
 {
-    FolderManager tenge = new FolderManager();
+    SimpleMethods sm= new SimpleMethods();
 
     public static void Main(string[] args)
     {
-        long startFplID = 1114702;
+       long startFplID = GetLeagueNumber("R2G");
+
         // six 153204
         // PovertyLeague 1089205
         // R2G 420969
         // h2h 153197 H
         // FPLwire 36074
+        // Arsenal 36074
         // BetssonLeague 1173870
         // KasbyLeague 190771
         // ComicsLeague 1114702
         //  FantasyShow 56013
-        Dictionary<string, string> leaguePlayerNames = GetLeaguePlayerNamesDictionary(startFplID, "c");
-        GetFplDetailsArray(leaguePlayerNames,6, "ComicsLeague");
+        string leagueName = GetLeagueNameByID(startFplID);
+        Dictionary<string, string> leaguePlayerNames;
+
+        if (leagueName == "h2h")
+        {
+            leaguePlayerNames = GetLeaguePlayerNamesDictionary(startFplID, "h");
+            GetFplDetailsArray(leaguePlayerNames, 7, leagueName);
+        }
+        else
+        {
+            leaguePlayerNames = GetLeaguePlayerNamesDictionary(startFplID, "c");
+            GetFplDetailsArray(leaguePlayerNames, 7, leagueName);
+        }
+
 
     }
-    
+
+
+    // Dictionary to hold the key-value pairs (with long values)
+    public static Dictionary<string, long> leagueData = new Dictionary<string, long>()
+    {
+        { "six", 153204L },
+        { "PovertyLeague", 1089205L },
+        { "R2G", 420969L },
+        { "h2h", 153197L },
+        { "India", 120 },
+        { "FPLwire", 36074L },
+        { "BetssonLeague", 1173870L },
+        { "KasbyLeague", 190771L },
+        { "ComicsLeague", 1114702L },
+        { "Overall", 314 },
+        { "Arsenal", 1 },
+        { "FantasyShow", 56013L }
+    };
+
+    public static string GetLeagueNameByID(long leagueId)
+    {
+        foreach (var kvp in leagueData)
+        {
+            if (kvp.Value == leagueId)
+            {
+                return kvp.Key; // Return the key associated with the leagueId
+            }
+        }
+        Console.WriteLine($"No league found with ID: {leagueId}");
+        return null;
+    }
 
     static long GetUnixTimestamp()
     {
@@ -92,72 +137,90 @@ public class FolderManager
 
         using (var driver = new ChromeDriver(options))
         {
-            foreach (var entry in leaguePlayerNames)
+            int LogCounter=0;
+            try
             {
-                string teamName = entry.Value;
-                string managerId = entry.Key;
-
-                // Navigate to the manager's FPL details page for the specified gameweek
-                driver.Navigate().GoToUrl($"https://fantasy.premierleague.com/entry/{managerId}/event/{gameweek}");
-                Thread.Sleep(3000); // Wait for the page to load
-                options.AddArgument("--headless");
-                // Extract country code image URL
-                string xPathCountryImg = "//div[@class='sc-bdnxRM hbrYOM']/img";
-                string xPathLatestPoints = "//div[@class='EntryEvent__PrimaryValue-sc-l17rqm-4 jsdnqB']";
-
-                var element1 = driver.FindElement(By.XPath(xPathCountryImg));
-                var elementLp = driver.FindElement(By.XPath(xPathLatestPoints));
-                string Lp = elementLp.Text;
-                string src = element1.GetAttribute("src");
-                string countryCode = ExtractCountryCode(src); // Custom method to extract country code
-
-                // Extract player details
-                string ply = "(//span[contains(@class,'styles__PitchElementData-sc-hv19ot-7 huoEoG')])";
-                var elementsWithClassName = driver.FindElements(By.XPath(ply));
-                List<string> playerNames = new List<string>();
-
-                for (int index = 0; index < elementsWithClassName.Count; index++)
+                foreach (var entry in leaguePlayerNames)
                 {
-                    var element = elementsWithClassName[index];
-                    string playerName = ConvertNewlineToSpace(element.Text); // Clean up player name
-                    playerNames.Add(playerName);
-                }
+                    string teamName = entry.Value;
+                    string managerId = entry.Key;
 
-                // Ensure the list has at least 15 players before accessing elements by index
-                if (playerNames.Count >= 15)
-                {
-                    fplDetailsList.Add(new JsonFPLMembers
+                    // Navigate to the manager's FPL details page for the specified gameweek
+                    driver.Navigate().GoToUrl($"https://fantasy.premierleague.com/entry/{managerId}/event/{gameweek}");
+                    Thread.Sleep(3000); // Wait for the page to load
+                    options.AddArgument("--headless");
+                    // Extract country code image URL
+                    string xPathCountryImg = "//div[@class='sc-bdnxRM hbrYOM']/img";
+                    string xPathLatestPoints = "//div[@class='EntryEvent__PrimaryValue-sc-l17rqm-4 jsdnqB']";
+
+                    var element1 = driver.FindElement(By.XPath(xPathCountryImg));
+                    var elementLp = driver.FindElement(By.XPath(xPathLatestPoints));
+                    string Lp = elementLp.Text;
+                    string src = element1.GetAttribute("src");
+                    string countryCode = ExtractCountryCode(src); // Custom method to extract country code
+
+                    // Extract player details
+                    string ply = "(//span[contains(@class,'styles__PitchElementData-sc-hv19ot-7 huoEoG')])";
+                    var elementsWithClassName = driver.FindElements(By.XPath(ply));
+                    List<string> playerNames = new List<string>();
+
+                    for (int index = 0; index < elementsWithClassName.Count; index++)
                     {
-                        manager_Name = managerId,
-                        Teams = teamName,
-                        Latp = Lp,
-                        Player_1 = playerNames[0],
-                        Player_2 = playerNames[1],
-                        Player_3 = playerNames[2],
-                        Player_4 = playerNames[3],
-                        Player_5 = playerNames[4],
-                        Player_6 = playerNames[5],
-                        Player_7 = playerNames[6],
-                        Player_8 = playerNames[7],
-                        Player_9 = playerNames[8], 
-                        Player_10 = playerNames[9],
-                        Player_11 = playerNames[10],
-                        Player_12 = playerNames[11],
-                        Player_13 = playerNames[12],
-                        Player_14 = playerNames[13],
-                        Player_15 = playerNames[14],
-                        SXL = countryCode // Add country code as additional info
-                        
+                        var element = elementsWithClassName[index];
+                        string playerName = ConvertNewlineToSpace(element.Text); // Clean up player name
+                        playerNames.Add(playerName);
+                    }
 
-                    });
-                    Console.WriteLine(teamName);
+                    // Ensure the list has at least 15 players before accessing elements by index
+                    if (playerNames.Count >= 15)
+                    {
+                        fplDetailsList.Add(new JsonFPLMembers
+                        {
+                            manager_Name = managerId,
+                            Teams = teamName,
+                            Latp = Lp,
+                            Player_1 = playerNames[0],
+                            Player_2 = playerNames[1],
+                            Player_3 = playerNames[2],
+                            Player_4 = playerNames[3],
+                            Player_5 = playerNames[4],
+                            Player_6 = playerNames[5],
+                            Player_7 = playerNames[6],
+                            Player_8 = playerNames[7],
+                            Player_9 = playerNames[8],
+                            Player_10 = playerNames[9],
+                            Player_11 = playerNames[10],
+                            Player_12 = playerNames[11],
+                            Player_13 = playerNames[12],
+                            Player_14 = playerNames[13],
+                            Player_15 = playerNames[14],
+                            SXL = countryCode // Add country code as additional info
+
+
+                        });
+                        LogCounter = LogCounter + 1;
+                        Console.WriteLine(LogCounter + " rank " + teamName);
+                    }
+                    /*
+                     * OpenQA.Selenium.NoSuchElementException: 'no such element: Unable to locate element: {"method":"xpath","selector":"//div[@class='sc-bdnxRM hbrYOM']/img"}
+                        (Session info: chrome=129.0.6668.91); For documentation on this error, please visit: https://www.selenium.dev/documentation/webdriver/troubleshooting/errors#no-such-element-exception'
+
+                     * */
                 }
             }
+            catch (Exception ex)
+            {
+                // General catch for any other exceptions
+                Console.WriteLine("An unexpected error occurred: " + ex.Message);
+            }
 
-            driver.Quit(); // Close the browser
-
-            // Example: Generate JS output and write to a file
-            string jsOutput = GenerateJsArray(fplDetailsList,LeagueName,gameweek);
+            finally
+            {
+                driver.Quit(); // Close the browser
+                // Example: Generate JS output and write to a file
+                string jsOutput = GenerateJsArray(fplDetailsList, LeagueName, gameweek);
+            }
+           
         }
     }
     public static string ConvertNewlineToSpace(string input)
@@ -249,6 +312,22 @@ public class FolderManager
             }
             driver.Quit();
             return playerData;
+        }
+    }
+
+
+    // Method to get the value by key (now returning long)
+    public static long GetLeagueNumber(string leagueName)
+    {
+        // Check if the dictionary contains the key
+        if (leagueData.TryGetValue(leagueName, out long leagueNumber))
+        {
+            return leagueNumber;
+        }
+        else
+        {
+            Console.WriteLine($"League name '{leagueName}' not found.");
+            return 90980; // Return a sentinel value to indicate the key was not found
         }
     }
 
